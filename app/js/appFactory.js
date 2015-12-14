295bec570cb374fab0f04cc35bb87dabdc7ad5ae
Jason Thomas <nanoman689@gmail.com>
2015-12-14 07:53:38 -0800

    12_14_15-753am

--- app/js/appFactory.js
+++ app/js/appFactory.js
@@ -1,7 +1,7 @@
 angular.module('myApp', ['ngRoute'])
 .constant('secret', 'f70ebe3932a951df')
 .constant('key', '9bf438c9008c14b50c8114ee607b8752')
-.factory("flickrLogIn", function($http) {
+.factory("FlickrService", function($http) {
 	return {
     	fetchPerson: function() {
       		//code to fetch person details
@@ -11,32 +11,31 @@ angular.module('myApp', ['ngRoute'])
             $scope.photos = [];
     	},
 		getToken: function(){
-			//code to get the security token
-            var key = "9bf438c9008c14b50c8114ee607b8752";
-            var secret = "f70ebe3932a951df";
-            var login = secret+"api_key"+key+"formatjsonfrob"+$scope.frob+"jsoncallbackangular.callbacks._0methodflickr.auth.getToken";
+	       var login = secret+"api_key"+key+"formatjsonfrob"+$scope.frob+"jsoncallbackangular.callbacks._0methodflickr.auth.getToken";
             var hash = md5(login);
             var data = {"method": "flickr.auth.getToken", 
-                        "api_key": "9bf438c9008c14b50c8114ee607b8752",
+				        "api_key": "9bf438c9008c14b50c8114ee607b8752",
                         "frob": $scope.frob,
                         "api_sig": hash,
                         "jsoncallback": "JSON_CALLBACK",
                         "format": "json"};
             var config = {
-                        "method":"GET",
+    			         "method":"GET",
                         "params":data,
                         "responseType": "JSONP"
-                        }
-		}
+            }
+	        var defer = $q.defer();
+                $http.jsonp('http://flickr.com/services/rest/', config)
+            
+        .then(function(data, status, headers, config) {
+		  defer.resolve(data.data.auth.token._content);
+	   });
+	       return defer.promise;   
+    }
   };
 })
 .controller("flickrController", function($scope, $http){
-	$scope.frob = (location.search.split('frob=')[1]||'').split('&')[0];
-	$scope.flickerLogIn=function(){
-            var login=secret+"api_key"+key+"permsread";
-            var hash = md5(login); 
-            console.log("The hash is:" + hash);
-            var url="http://flickr.com/services/auth/?api_key="+key+"&perms=read&api_sig="+hash;
-            window.location=url; 
-     }
+        flickrLogin.getToken().then(function(token){
+            $scope.token = token;
+        })
 });	
