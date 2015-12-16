c230d60a1ee5e6da0184758d448006a78e0f96b6
Jason Thomas <nanoman689@gmail.com>
2015-12-16 07:15:28 -0800

    12_16_15_715am

--- app/js/appFactory.js
+++ app/js/appFactory.js
@@ -10,12 +10,12 @@ angular.module('myApp', ['ngRoute'])
       		//code to fetch photos of the person
             $scope.photos = [];
     	},
-		getToken: function(){
-	        var login = secret+"api_key"+key+"formatjsonfrob"+$scope.frob+"jsoncallbackangular.callbacks._0methodflickr.auth.getToken";
+		getToken: function(frob){  
+	        var login = secret+"api_key"+key+"formatjsonfrob"+frob+"jsoncallbackangular.callbacks._0methodflickr.auth.getToken";
             var hash = md5(login);
             var data = {"method": "flickr.auth.getToken", 
 				        "api_key": "9bf438c9008c14b50c8114ee607b8752",
-                        "frob": $scope.frob,
+                        "frob": frob,
                         "api_sig": hash,
                         "jsoncallback": "JSON_CALLBACK",
                         "format": "json"};
@@ -35,9 +35,24 @@ angular.module('myApp', ['ngRoute'])
     }
   };
 })
-.controller("flickrController", function($scope, $http){
+.controller("flickrController", function(flickrService, $scope, $http){
         // User clicks on button to log into Flickr 
-        $scope.flickrService.getToken().then(function(token){
-            $scope.token = token;
+    $scope.frob = (location.search.split('frob=')[1]||'').split('&')[0];
+    
+    if($scope.frob !== ''){
+        flickrService.getToken($scope.frob).then(function(token){
+        $scope.token = token;
+        console.log($scope.token);
         })
+    }
+    
+    $scope.flickerLogIn=function(){
+        var key="9bf438c9008c14b50c8114ee607b8752";
+        var secret="f70ebe3932a951df";
+        var login=secret+"api_key"+key+"permsread";
+        var hash = md5(login); 
+        console.log("The hash is:" + hash);
+        var url="http://flickr.com/services/auth/?api_key="+key+"&perms=read&api_sig="+hash;
+        window.location=url; 
+    }
 });	
