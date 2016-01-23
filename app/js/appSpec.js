befddaa084f8179b61953aec804b1e6f0ad78011
Jason Thomas <nanoman689@gmail.com>
2016-01-22 19:00:20 -0800

    1-22-16-700pm

--- app/js/appSpec.js
+++ app/js/appSpec.js
@@ -1,3 +1,4 @@
+/*
 describe('flickrService', function() {
     beforeEach(module('myApp'));
 
@@ -14,3 +15,39 @@ describe('flickrService', function() {
         $httpBackend.verifyNoOutstandingRequest();
     }));
 });
+*/
+
+/* testing factory */
+
+describe("flickrService", function() {
+
+  beforeEach(module('myApp'));
+
+  it('should contain an flickrService service',
+    inject(function(flickrService) {
+    expect(flickrService).not.toBe(null);
+  }));
+
+});
+
+/* controller test */
+
+describe("myApp", function() {
+    beforeEach(module('myApp'));
+    
+    var ctrl, scope;
+    beforeEach(inject(function($scope, $routeParams, $rootScope){
+        scope = $rootScope.$new();
+        ctrl = $controller('flickrController',{
+              $scope : scope        
+        });                     
+    }));
+    
+    it('should return a empty frob',
+        inject(function(){
+            expect(scope.frob).toBe("")
+        
+        }));  
+});
+
+/* service test */ 
